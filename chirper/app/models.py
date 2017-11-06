617024d5352a0a4d09867f5e4a90d01e89b74001
Nate Clark <natec425@gmail.com>
2017-11-06 15:52:02 -0600

    Add draft for ChirperUser and Chirp models

--- chirper/app/models.py
+++ chirper/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ChirperUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300, blank=True)
+    location = models.CharField(max_length=50, blank=True)
+    website = models.URLField(blank=True)
+    joined = models.DateField(auto_now_add=True)
+
+
+class Chirp(models.Model):
+    message = models.CharField(max_length=280)
+    author = models.ForeignKey(ChirperUser, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)

69b836d8071c99b18176ef013d1fbc133910d530
Nate Clark <natec425@gmail.com>
2017-11-10 10:39:13 -0600

    Remove session related apps/middleware.
    This is the start of replacing the default session implementation
    with a token based scheme that is suitable for a json api.
    Goals:
        - new session store
        - new authentication middleware
            - Require token to be passed

--- chirper/settings.py
+++ chirper/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
 ]
 
 if not os.environ.get('DEBUG', False):
@@ -46,10 +44,7 @@ if not os.environ.get('DEBUG', False):
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
